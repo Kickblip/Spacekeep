3613360dcfbf339d3edbd8d04abe60d55732cc07
Kickblip <wyatt.zilker@gmail.com>
2023-09-30 14:11:39 -0600

    asteroid bounces off ship when hit

--- Ludum Dare 54/Assets/Asteroid_Mining.cs
+++ Ludum Dare 54/Assets/Asteroid_Mining.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     public int hp = 100;  // Asteroid HP
+    public float explosionForce = 10f;
+    public float explosionRadius = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -15,6 +24,14 @@ public class Asteroid : MonoBehaviour
             {
                 Debug.Log("Asteroid destroyed");
                 Destroy(gameObject);
+            }
+            else
+            {
+                Vector2 explosionCenter = collision.contacts[0].point;
+                Vector2 direction = (rb.position - explosionCenter).normalized;
+                float distance = Vector2.Distance(rb.position, explosionCenter);
+                float force = explosionForce / (1 + distance / explosionRadius);
+                rb.AddForce(direction * force, ForceMode2D.Impulse);
             }
         }
     }
