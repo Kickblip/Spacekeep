f3b3f14aeecac821871b1454916bce3ba64905cc
chip03 <111972339+chip003@users.noreply.github.com>
2023-09-30 22:29:50 -0600

    Add inventory
    Added an inventory menu the player can access with "I". Need to add more code for checking if a slot is already occupied. Inventory grid size can be changed by setting the serialized width and height variables.

---
+++ Ludum Dare 54/Assets/Jesse's Changes/GridSystem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour
+{
+    [SerializeField] int width;
+    [SerializeField] int height;
+    float tileSize = 0.16f;
+    Camera cam;
+    SpriteRenderer render;
+
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        render = GetComponent<SpriteRenderer>();
+        render.size = new Vector2(width*tileSize,height*tileSize);
+
+        Vector2 S = render.size;
+        GetComponent<BoxCollider2D>().size = S;
+        GetComponent<BoxCollider2D>().offset = new Vector2 ((S.x/2), (S.y/2));
+    }
+
+    //convert mouse coords to closest grid coordinates
+    public Vector2 getGridInt() {
+        Vector2 mp = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        float sizeCorrect = tileSize*transform.localScale.x;
+
+        float posX = Mathf.Floor((mp.x - transform.position.x)/sizeCorrect);
+        float posY = Mathf.Floor((mp.y - transform.position.y)/sizeCorrect);
+
+        return new Vector2(posX,posY);
+    }
+
+    public void Update() {
+        //Debug.Log(getGridInt());
+    }
+}
