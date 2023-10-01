c66530027a8e955f1ea0a4a5147786d951cd1b35
Alex de la Chica <80286400+Alexdlc5@users.noreply.github.com>
2023-10-01 14:14:12 -0600

    Finished Asteroid Generation

--- Ludum Dare 54/Assets/Asteroid_Field_Generator.cs
+++ Ludum Dare 54/Assets/Asteroid_Field_Generator.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class Asteroid_Field_Generator : MonoBehaviour
 {
     public GameObject[] asteroids;
+    public float[] type_spawn_boundrys = {.25f, .5f, .75f};
     public float outer_boundry;
     public float inner_boundry;
     public float generation_density;
@@ -22,10 +24,53 @@ public class Asteroid_Field_Generator : MonoBehaviour
                 float distance_from_center = Mathf.Sqrt(Mathf.Pow(random_position.x, 2) + Mathf.Pow(random_position.y, 2));
                 if (distance_from_center < outer_boundry && distance_from_center > inner_boundry)
                 {
-                    //select astroid type *0 index is temp*
-                    Instantiate(asteroids[0], random_position, transform.rotation); // change to be random rotation
+                    Instantiate(asteroids[selectType(distance_from_center)], random_position, transform.rotation); //change to be random rotation
                     good_gen_pos = true;
                 }
+            }
+        }
+    }
+    private int selectType(float distance_from_center)
+    {
+        float random = Random.Range(1, 11);
+        if (distance_from_center / outer_boundry < type_spawn_boundrys[0])
+        {
+            return 0;
+        }
+        if (distance_from_center / outer_boundry < type_spawn_boundrys[1])
+        {
+            //select astroid 
+            if (random > 6.5f)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        if (distance_from_center / outer_boundry < type_spawn_boundrys[2])
+        {
+            //select astroid 
+            if (random > 6.5f)
+            {
+                return 1;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+        else
+        {
+            //select astroid 
+            if (random > 6.5f)
+            {
+                return 2;
+            }
+            else
+            {
+                return 3;
             }
         }
     }
