67cd9c08e4f1414b05dd0939319b084c8bef2712
19tylermalone94 <19tylermalone94@gmail.com>
2023-10-01 23:05:30 -0600

    enemy prefab

---
+++ Ludum Dare 54/Assets/EnemyController.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform motherShipPos;  // Drag your mother ship here in the inspector
+    public float moveSpeed = 5.0f;
+    public int hp = 100;  // Asteroid HP
+    public float explosionForce = 10f;
+    public float explosionRadius = 5f;
+    public OreType[] possibleOreTypes;  // Possible ore types to spawn
+    public int max_allowed_ore_types;
+    public GameObject oreChunckPrefab; // Ore chunk prefab
+    public int numberOfChunks = 3;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+
+    private void Update()
+    {
+        if (motherShipPos != null)
+        {
+            Vector2 direction = (motherShipPos.position - transform.position).normalized;
+            if (rb)
+            {
+                rb.velocity = direction * moveSpeed;
+            }
+            else
+            {
+                transform.position += (Vector3)direction * moveSpeed * Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            hp -= 10;
+            Debug.Log("Enemy hit by player. HP: " + hp);  // log the current hp
+
+            if (hp <= 0)
+            {
+                Debug.Log("Enemy destroyed");
+                SpawnDebris();
+                Destroy(gameObject);
+            }
+            else
+            {
+                Vector2 explosionCenter = collision.contacts[0].point;
+                Vector2 direction = (rb.position - explosionCenter).normalized;
+                float distance = Vector2.Distance(rb.position, explosionCenter);
+                float force = explosionForce / (1 + distance / explosionRadius);
+                rb.AddForce(direction * force, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void SpawnDebris()
+    {
+        for (int i = 0; i < numberOfChunks; i++)
+        {
+            // Add an ore chunk!
+            GameObject chunk = Instantiate(oreChunckPrefab, transform.position, Quaternion.identity);
+            
+            // Assign one of the possible ore types to this chunk
+            OreChunk oreChunk = chunk.GetComponent<OreChunk>();
+            if (oreChunk != null && possibleOreTypes.Length > 0)
+            {
+                int randomIndex = Random.Range(0, max_allowed_ore_types);
+                oreChunk.oreType = possibleOreTypes[randomIndex];
+            }
+
+            // Add some force to make the chunks explode outwards
+            Rigidbody2D chunkRb = chunk.GetComponent<Rigidbody2D>();
+            if (chunkRb != null)
+            {
+                Vector2 randomDirection = Random.insideUnitCircle.normalized;
+                float randomForce = Random.Range(explosionForce / 2f, explosionForce);
+                chunkRb.AddForce(randomDirection * randomForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
