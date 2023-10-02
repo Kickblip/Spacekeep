d8e3af9bc99d57a33714e8abba1735106199b345
Alex de la Chica <80286400+Alexdlc5@users.noreply.github.com>
2023-10-02 14:58:48 -0600

    GO patch

--- Ludum Dare 54/Assets/Scripts/MothershipController.cs
+++ Ludum Dare 54/Assets/Scripts/MothershipController.cs
@@ -6,12 +6,10 @@ using UnityEngine.SceneManagement;
 public class MothershipController : MonoBehaviour
 {
     public float hp = 500;
-    public GameObject game_over_screen;
 
     void Update() {
         if (hp <= 0) {
             Debug.Log("Mothership Destroyed!");
-            game_over_screen.SetActive(true);
             Destroy(gameObject);
             SceneManager.LoadScene(3);
         }
