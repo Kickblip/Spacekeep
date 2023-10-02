eedb37d987834a6b156bc9be1b0a86084fc46527
Alex de la Chica <80286400+Alexdlc5@users.noreply.github.com>
2023-10-02 14:37:59 -0600

    Game Over

--- Ludum Dare 54/Assets/Scripts/MothershipController.cs
+++ Ludum Dare 54/Assets/Scripts/MothershipController.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MothershipController : MonoBehaviour
 {
     public float hp = 500;
+    public GameObject game_over_screen;
 
     void Update() {
         if (hp <= 0) {
             Debug.Log("Mothership Destroyed!");
+            game_over_screen.SetActive(true);
             Destroy(gameObject);
+            SceneManager.LoadScene(3);
         }
     }
 }
